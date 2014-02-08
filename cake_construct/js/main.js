f7c17f56203fdb6a3ee183975d9aa9930768c371
Eli <temp@temps-MacBook-Pro.local>
2014-02-08 15:36:35 -0500

    app html and cb pdf updates

--- cake_construct/js/main.js
+++ cake_construct/js/main.js
@@ -1,6 +1,12 @@
 //jquery syntax
-$(document).ready(function(){
+$('window').ready(function(){
+
 	$('.member-entry').on('click',entry({
-		console.log("wtf");
-	}))
+		console.log("hey");
+		// var entryForm= $('.entryform').length;
+		// if(entryform===0){
+		// 	$('.popout').show();
+		// 	$('#login').show();
+		// };
+	});
 });
