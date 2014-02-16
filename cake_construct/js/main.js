22d4c2905c317df2016c02318747b0e8b7d87bbc
Eli <temp@unknown1040f39eb0f4.gateway.pace.com>
2014-02-15 23:58:56 -0500

    hand in for 2nd milestone

--- cake_construct/js/main.js
+++ cake_construct/js/main.js
@@ -30,4 +30,10 @@ $(document).ready(function(){
 			}
 		}
 	});
+	$("#carousel").waterwheelCarousel({
+      // include options like this:
+      // (use quotes only for string values, and no trailing comma after last option)
+      // option: value,
+      // option: value
+    });
 });
