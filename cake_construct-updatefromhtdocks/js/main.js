90b685dea75764beee0b2a152ad7f831c3acadfa
Eli <temp@temps-MacBook-Pro.local>
2014-02-22 02:36:32 -0500

    replaced form hdocks in mamp

---
+++ cake_construct-updatefromhtdocks/js/main.js
@@ -0,0 +1,74 @@
+//jquery syntax
+$(document).ready(function(){
+	$('.member-entry').click(function(){
+		$(".popout").toggle();
+	});
+	$('.module').hide({height:100},1000);
+	$('.tabmodule').before('<ul class="tabings"></ul>');
+	$('.module').each(function(){
+		$('.tabings').append('<li class="listing" ><h2>'+$(this).attr('id')+'</h2></li>');
+	});
+	$('.listing').click(function(){
+		if($(this).find('h2').html()=='listing active'){
+			$(this).removeClass('active');
+		}else{
+			$(this).addClass('active');
+			if($(this).find('h2').html()=='wedding'){
+				$('#wedding').show();
+				$('#birthday').hide()
+				$('#corperate').hide()
+			}else if($(this).find('h2').html()=='birthday'){
+				$('#birthday').show();
+				$('#wedding').hide();
+				$('#corperate').hide();
+			}else if($(this).find('h2').html()=='corperate'){
+				$('#corperate').show();
+				$('#wedding').hide();
+				$('#birthday').hide();
+			}
+		}
+	});
+	$("#carousel").waterwheelCarousel({
+   		
+    });
+//values captured for use in login
+    var username=$('#name').val();
+    var password=$('#password').val();
+    $("#current-submit").click(function(){
+	    var username=$('#name').val();
+    	var password=$('#password').val();
+    	$.ajax({
+			url:'xhr/login.php',
+			type:'post',
+			dataType:'json',
+			data:{
+			username:username,
+			password:password
+			},
+			success:function(response){
+				console.log(response.user+"works");
+				window.location.assign("app.html");
+			}//object has no closing; 
+		});
+    });
+    var checkLogin = function(){
+	    $.ajax({
+	            url: 'xhr/check_login.php',
+	            type: 'get',
+	            dataType: 'json',
+	            success: function(response){
+	            	console.log(response);
+	                    if(response.user){
+	                            window.location.assign("app.html");
+	                    } else {
+	                            window.location.assign("index.html");
+	                    }
+	            }
+	    });
+    };
+    var init=function(){
+    	checkLogin();
+    }
+    //init(); i know why its repeating endlessly but not sure i remmber how to correctly have it check login.
+
+});
