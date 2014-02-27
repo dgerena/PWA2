5f82b96d426db1e0a4cc7f2b7073aa68558ad7f2
Eli <temp@temps-MacBook-Pro.local>
2014-02-27 10:07:04 -0500

    Have been editing in my ht docs
    Forgot to pull my folder from htdocks  for use on mamp to my pwa 2
    folder.

--- cake_construct/js/main.js
+++ cake_construct/js/main.js
@@ -1,5 +1,14 @@
 //jquery syntax
 $(document).ready(function(){
+	$(document).load(function(){
+		$.get('xhr/check_login.php',function(response){
+			if (response.error) {
+				window.location.assign("index.html")
+			}else{
+	    		window.location.assign("app.html");
+	    	}
+	    });
+	});
 	$('.member-entry').click(function(){
 		$(".popout").toggle();
 	});
@@ -31,52 +40,77 @@ $(document).ready(function(){
 	$("#carousel").waterwheelCarousel({
    		
     });
-//values captured for use in login
-    var username=$('#name').val();
-    var password=$('#password').val();
+	//values captured for use in login
     $("#current-submit").click(function(){
-	    var username=$('#name').val();
-    	var password=$('#password').val();
     	$.ajax({
 			url:'xhr/login.php',
 			type:'post',
 			dataType:'json',
 			data:{
-			username:username,
-			password:password
+			username:$('#name').val(),
+			password:$('#password').val()
 			},
 			success:function(response){
-				console.log(response.user+"works");
-				window.location.assign("app.html");
+				if(response.error) {
+					alert(response.error);
+				}else{
+					window.location.assign("app.html");
+				};
 			}//object has no closing; 
 		});
     });
-    // $('#logout').click(function(){
-    // 	window.location.reload(true);
-    // });
-    var checkLogin = function(){
-    	if ($(document).location.href== window.location.href('http://localhost:8888/cake_construct/app.html')){
-		    $.ajax({
-		            url: 'xhr/check_login.php',
-		            type: 'get',
-		            dataType: 'json',
-		            success: function(response){
-		            	console.log(response);
-		                    if(response.user){
-		                            window.location.assign("app.html");
-		                    } else {
-		                            window.location.reload(true);
-		                    }
-		            }
-		    });
-		};
-	    console.log("lol");
-    };
-    // checkLogin();
-    // var init=function(){
-    // 	if (window.location.value) {};
-    // 	checkLogin();
-    // }
-    // init(); i know why its repeating endlessly but not sure i remmber how to correctly have it check login.
+    $('#reg-submit').click(function(){
+    	var nameReg=$('#name-reg').val();
+    	var email=$('#email').val();
+    	var passReg=$('#reg-password').val();
+    	var passVal=$('#validate').val();
+    	$.ajax({
+    		url:'xhr/register.php',
+    		type:'post',
+    		dataType:'json',
+    		data:{
+				username:nameReg,
+				password:passreg||passVal,
+				email:email
+    		},
+    		success:function(response){
+    			if (response.error) {
+    				console.log('error '+response.error);
+    			}else{
+    				window.location.assign("app.html");
+    			};
+    		}
 
+    	});
+    });
+    $('#logout').click(function(){
+    	$.get('xhr/logout.php',function(){
+    		window.location.assign("index.html");
+    	});
+    });
+
+    $(function() {
+	    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+	    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+  		$('.carousel-center').click(function(){
+    		$('html').css();
+    	});
+    	$('#accordion').accordion({
+    		heightStyle:"content"
+    	});
+  	});
+  	/* -----------------------------form fields*/
+  	/* range slider value and a field input to type specific number*/
+  	$('.rangeslide').on('click mouseout',function(){
+		var val=$(this).val();
+
+		$('.guest_num').val(val);
+		console.log(this.value)
+    });
+   	$('.guest_num').on('keyup',function(){
+		var val=$(this).val();
+		$('.rangeslide').val(val);
+		console.log(this.value);
+    });
+   	
 });
